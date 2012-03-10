281f07fcfa84c9471468496db3d978863f7fc0a9
Olivier Girardot <ssaboum@gmail.com>
2012-03-10 12:03:08 +0100

    mockup ressource scaffolding + began repository interfaces

--- devinlove/src/main/java/com/lateralthoughts/devinlove/bean/Mascot.java
+++ devinlove/src/main/java/com/lateralthoughts/devinlove/bean/Mascot.java
@@ -1,7 +1,18 @@
 package com.lateralthoughts.devinlove.bean;
 
+import org.springframework.data.neo4j.annotation.GraphId;
+import org.springframework.data.neo4j.annotation.Indexed;
+import org.springframework.data.neo4j.annotation.NodeEntity;
+
 /**
  * Mascot ! Because everything has a Mascot !
  */
+@NodeEntity
 public class Mascot {
+    
+    @GraphId Long id;
+    
+    //@Indexed(type="FULLTEXT", indexName = "search")
+    String name;
+
 }
---
+++ devinlove/src/main/java/com/lateralthoughts/devinlove/bean/repository/MeetupRepository.java
@@ -0,0 +1,14 @@
+package com.lateralthoughts.devinlove.bean.repository;
+
+import com.lateralthoughts.devinlove.bean.Meetup;
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ogirardot
+ * Date: 10/03/12
+ * Time: 12:00
+ * To change this template use File | Settings | File Templates.
+ */
+public interface MeetupRepository extends GraphRepository<Meetup>{
+}
