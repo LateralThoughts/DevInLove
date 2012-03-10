495fe641bb6dcb21195bc196acff7a8dcf851489
Olivier Girardot <ssaboum@gmail.com>
2012-03-10 12:20:04 +0100

    began working on spring mvc and defining default controller

---
+++ devinlove/src/main/java/com/lateralthoughts/devinlove/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.lateralthoughts.devinlove.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ogirardot
+ * Date: 10/03/12
+ * Time: 12:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class HomeController implements  {
+}
