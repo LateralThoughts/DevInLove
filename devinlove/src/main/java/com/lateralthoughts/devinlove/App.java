ac743103c2579aa5eaca252a280749b8c832d51f
Olivier Girardot <ssaboum@gmail.com>
2012-03-10 11:38:48 +0100

    archetype entry point

---
+++ devinlove/src/main/java/com/lateralthoughts/devinlove/App.java
@@ -0,0 +1,11 @@
+package com.lateralthoughts.devinlove;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ogirardot
+ * Date: 10/03/12
+ * Time: 11:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class App {
+}
