96fbdd1b70ed5525670034c917b0bc3743b310ac
Olivier Girardot <ssaboum@gmail.com>
2012-03-15 22:45:07 +0100

    good bean mascot good bean

--- devinlove/src/main/java/com/lateralthoughts/devinlove/domain/Mascot.java
+++ devinlove/src/main/java/com/lateralthoughts/devinlove/domain/Mascot.java
@@ -13,4 +13,32 @@ public class Mascot {
     public String name;
 
     public Mascot() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Mascot mascot = (Mascot) o;
+
+        if (id != null ? !id.equals(mascot.id) : mascot.id != null) return false;
+        if (name != null ? !name.equals(mascot.name) : mascot.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Mascot{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
