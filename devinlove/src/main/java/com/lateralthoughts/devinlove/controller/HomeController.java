d1c8efb973cd884ff1f74c2392c9897b61332990
Florent Biville <f.biville@lateral-thoughts.com>
2012-03-15 22:39:29 +0100

    Added view resolver and auto-scan in Jetty configuration

--- devinlove/src/main/java/com/lateralthoughts/devinlove/controller/HomeController.java
+++ devinlove/src/main/java/com/lateralthoughts/devinlove/controller/HomeController.java
@@ -1,15 +1,14 @@
 package com.lateralthoughts.devinlove.controller;
 
-import com.lateralthoughts.devinlove.domain.Mascot;
-import com.lateralthoughts.devinlove.repository.MascotRepository;
+import static java.util.Arrays.asList;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.neo4j.support.Neo4jTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
+import com.lateralthoughts.devinlove.domain.Mascot;
+import com.lateralthoughts.devinlove.repository.MascotRepository;
 
 @Controller
 public class HomeController {
@@ -17,12 +16,12 @@ public class HomeController {
     MascotRepository mascotRepository;
 
     @RequestMapping(value = "/index.html")
-    public String index(Model model) throws Exception {
-        Mascot m = new Mascot();
-        m.name = "Django Pony";
-        mascotRepository.save(m);
+    public String index(final Model model) throws Exception {
+        Mascot mascot = new Mascot();
+        mascot.name = "Django Pony";
+        mascotRepository.save(mascot);
         model.addAttribute("message", "test");
-        model.addAttribute("latestMascots", Arrays.asList(new String[]{"a", "b", "c"}));
-        return "jsp/index.jsp";
+		model.addAttribute("latestMascots", asList(new String[] { "a", "b", "c" }));
+		return "index";
     }
 }
